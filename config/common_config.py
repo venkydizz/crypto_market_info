58d8482db4506808320773eea6313657f41645b2
venkateshs <venkateshs@applieddatafinance.com>
2023-07-01 14:11:26 +0530

    Created two new endpoints market_summary_all, market_summary_for with basic implementation of authorization and validations. Also created logging feature

---
+++ config/common_config.py
@@ -0,0 +1,7 @@
+# Place to store configuration information for the application
+
+# SECURITY WARNING: keep the secret key used in production secret!
+BITTREX_URL = 'https://api.bittrex.com/v3/'
+BITTREX_KEY = 'bittrex_key'
+
+CLIENT_KEY = 'ab40713d69'
--- crypto_market_info/settings.py
+++ crypto_market_info/settings.py
@@ -122,3 +122,23 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+        'file': {
+            'class': 'logging.FileHandler',
+            'filename': 'logs/requests.log',
+        },
+    },
+    'root': {
+        'handlers': ['console', 'file'],
+        'level': 'INFO', 
+    },
+}
+
