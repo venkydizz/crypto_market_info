b00e49c524a7feb22c2fb4ecb702ee1ca1e7a091
venkateshs <venkateshs@applieddatafinance.com>
2023-07-01 21:38:20 +0530

    Client key is removed from config file and fetched from environment varible securely.

--- config/common_config.py
+++ config/common_config.py
@@ -1,11 +1,13 @@
 # Place to store configuration information for the application
+import os
 
 BITTREX_URL = 'https://api.bittrex.com/v3/'
 ENDPOINTS = {
     'market_summary': f"{BITTREX_URL}markets/summaries",
     'market_summary_all': f"{BITTREX_URL}markets/<market>/summary",
 }
-CLIENT_KEY = 'ab40713d69'
+# make sure to set the environment variables in the system
+CLIENT_KEY = os.environ.get('CLIENT_KEY')
 
 COMMON_RESPONSE = {
     '405': {"message": "Method Not Allowed"},
