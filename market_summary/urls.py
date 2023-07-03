9e4a6b3a0958e2be7131640e522212c161984479
venkateshs <venkateshs@applieddatafinance.com>
2023-07-03 23:02:58 +0530

    added crsf_exempt to ping and version views

--- market_summary/urls.py
+++ market_summary/urls.py
@@ -7,8 +7,8 @@ from . import views
 
 
 urlpatterns = [
-    path('ping/', views.ping, name='ping'),
-    path('version/', views.version, name='version'),
+    path('ping/', csrf_exempt(views.ping), name='ping'),
+    path('version/', csrf_exempt(views.version), name='version'),
     path('market_summary/all/', csrf_exempt(views.market_summary_all), name='market_summary_all'),
     path('market_summary/for/', csrf_exempt(views.market_summary_for), name='market_summary_for'),
 ]
